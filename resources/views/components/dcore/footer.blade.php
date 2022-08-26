e311233aad1b49e4f74d9e870bf6ab45ffd2f2e5
imyhacker <arifarhan1602@gmail.com>
2022-08-27 04:40:27 +0700

    Update

--- resources/views/components/dcore/footer.blade.php
+++ resources/views/components/dcore/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div>AMIK PN        </div>
+          Copyright &copy; 2022 <div class="bullet"></div>AMIK PN        </div>
         <div class="footer-right">
           
         </div>

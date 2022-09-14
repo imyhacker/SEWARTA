0317f9abbc566bac5c05eb6ce8b71d32b854e5ef
imyhacker <arifarhan1602@gmail.com>
2022-09-14 07:17:52 +0700

    Update sampai card dashboard

--- resources/views/components/dcore/card.blade.php
+++ resources/views/components/dcore/card.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
   
-    <div class="col-lg-6 col-md-6 col-sm-12">
+    <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="card card-statistic-2">
            
             <div class="card-icon shadow-primary bg-primary">
@@ -12,22 +12,6 @@
                 </div>
                 <div class="card-body">
                     {{$tb}}
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-12">
-        <div class="card card-statistic-2">
-            
-            <div class="card-icon shadow-primary bg-primary">
-                <i class="fas fa-book"></i>
-            </div>
-            <div class="card-wrap">
-                <div class="card-header">
-                    <h4>Total Informasi</h4>
-                </div>
-                <div class="card-body">
-                {{$ti}}
                 </div>
             </div>
         </div>
